397443e8e8d87c6f48a737ca4e3a56db1032d010
Károly Szabó <karoly.szabo@stud.uni-corvinus.hu>
2021-04-15 13:33:21 +0200

    gyak8_mbc69o

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/Legyenszépnapja")]
+        public IActionResult M2(string szoveg)
+        {
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = szoveg.ToUpper()
+            };
+        }
+        
+    }
+}
